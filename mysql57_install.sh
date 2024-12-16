0eb2da8f2b5530b148bc3c8d4d4322b49a1a1c53
minlearnminlearn <minlearn@minlearn.org>
2024-12-16 09:28:33 +0800

    Update mysql57_install.sh

--- mysql57_install.sh
+++ mysql57_install.sh
@@ -26,7 +26,7 @@ echo "Installed MySQL"
 echo "Configure MySQL Server"
 ADMIN_PASS="$(openssl rand -base64 18 | cut -c1-13)"
 mysql -uroot -p"$ADMIN_PASS" -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH $RELEASE_AUTH BY '$ADMIN_PASS'; FLUSH PRIVILEGES;"
-mysql -uroot -p"$ADMIN_PASS" -e "CREATE USER 'root'@'10.10.10.%' IDENTIFIED WITH $RELEASE_AUTH BY '$ADMIN_PASS'; GRANT ALL PRIVILEGES ON * . * TO 'root'@'10.10.10.%'; FLUSH PRIVILEGES;"
+mysql -uroot -p"$ADMIN_PASS" -e "CREATE USER 'root'@'10.10.10.%' IDENTIFIED WITH $RELEASE_AUTH BY '$ADMIN_PASS'; GRANT ALL PRIVILEGES ON * . * TO 'root'@'10.10.10.%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
 echo "" >~/mysql.creds
 echo -e "MySQL user: root" >>~/mysql.creds
 echo -e "MySQL password: $ADMIN_PASS" >>~/mysql.creds
